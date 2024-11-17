ba602894f7222c3988cf1e80a56aa297e7de70a3
Cruz Ulises Larraga Ramirez <uliseslarraga@gmail.com>
2024-11-16 22:44:38 -0600

    feat: Add command to create kubeconfig (#8)

--- main.tf
+++ main.tf
@@ -34,5 +34,8 @@ module "k8s" {
     source            = "./modules/k8s"
     tags              = var.tags
     environment       = var.environment
+    aws_region        = var.aws_region
+    cluster_name      = module.eks.cluster_name
     eks_role_irsa_arn = module.iam.eks_role_irsa_arn
+    depends_on        = [module.eks]
 }
