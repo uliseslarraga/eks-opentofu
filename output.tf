031b9dbc0b8b82f0dda32ba28c945f981f485088
Ulises Lárraga <uliseslarraga@gmail.com>
2024-12-08 14:24:54 -0600

    feat: Add database module to root module

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "cluster_name" {
 output "cluster_endpoint" {
     value = module.eks.cluster_endpoint
 }
+
+output "rds_endpoint" {
+    value = module.database.cluster_endpoint
+}
+
+output "rds_identifier" {
+    value = module.database.cluster_identifier
+}
