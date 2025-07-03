9c9c145040d3fe3b5c39b7485e7e709570fa5862
Ulises Lárraga <uliseslarraga@gmail.com>
2025-07-02 18:44:10 -0600

    feat: Add k8s service account for autoscaler

--- modules/k8s/variables.tf
+++ modules/k8s/variables.tf
@@ -20,5 +20,10 @@ variable "environment" {
 
 variable "eks_role_irsa_arn" {
   type        = string
-  description = "IAM Role for Kubernetes Service Account"
+  description = "AWS Controller ARN Role for Kubernetes Service Account"
+}
+
+variable "autoscaler_role_irsa_arn" {
+  type        = string
+  description = "Autoscaler ARN Role for Kubernetes Service Account"
 }
