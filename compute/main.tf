7db3d56f4a2b64377c77e4b802f361ff9f762a6e
Ulises Lárraga <uliseslarraga@gmail.com>
2025-07-06 23:18:32 -0600

    feat: Add conditional counts for RDS and Bastion host

--- compute/main.tf
+++ compute/main.tf
@@ -1,5 +1,6 @@
 module "bastion" {
     source           = "../modules/bastionhost"
+    count            = var.create_bastion ? 1 : 0
     vpc_id           = data.terraform_remote_state.networking.outputs.vpc_id
     environment      = var.environment
     public_subnet_id = data.terraform_remote_state.networking.outputs.pub_subnet_ids[0]
@@ -33,12 +34,13 @@ module "eks" {
 
 module "database" {
     source                = "../modules/database"
+    count                 = var.create_rds ? 1 : 0
     tags                  = merge({env = terraform.workspace}, var.tags) 
     environment           = var.environment
     vpc_id                = data.terraform_remote_state.networking.outputs.vpc_id
     private_data_subnets  = data.terraform_remote_state.networking.outputs.priv_data_subnet_ids
     private_subnets_cidrs = data.terraform_remote_state.networking.outputs.priv_subnet_cidrs
-    bastionh_sg_id        = module.bastion.bastionh_sg_id
+    bastionh_sg_id        = module.bastion[0].bastionh_sg_id
     worker_nodes_sg_id    = module.eks.worker_nodes_sg_id
 }
 
