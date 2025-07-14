d872bd2026a8121f3e7be378c4b48dd2cfdb81f5
Ulises Larraga <uliseslarraga@gmail.com>
2025-07-13 21:57:47 -0600

    feat: Add pod identity addon

--- modules/eks/addons.tf
+++ modules/eks/addons.tf
@@ -10,7 +10,7 @@ resource "aws_eks_addon" "vpc-cni" {
   ]
   cluster_name      = aws_eks_cluster.cluster.name
   addon_name        = "vpc-cni"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
   configuration_values = file("${local.cni_config}/cni.json")
   addon_version        = var.vpc_cni_version
   preserve = true
@@ -24,7 +24,7 @@ resource "aws_eks_addon" "core-dns" {
   ]
   cluster_name      = aws_eks_cluster.cluster.name
   addon_name        = "coredns"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
   configuration_values = file("${local.cni_config}/coredns.json")
   addon_version        = var.coredns_version
   preserve = true
@@ -37,8 +37,15 @@ resource "aws_eks_addon" "kube-proxy" {
   ]
   cluster_name      = aws_eks_cluster.cluster.name
   addon_name        = "kube-proxy"
-  resolve_conflicts = "OVERWRITE"
+  resolve_conflicts_on_create = "OVERWRITE"
   addon_version        = var.kube_proxy_version
   preserve = true
 
+}
+
+resource "aws_eks_addon" "pod_identity" {
+  cluster_name                = aws_eks_cluster.cluster.name
+  addon_name                  = "eks-pod-identity-agent"
+  resolve_conflicts_on_create = "OVERWRITE"
+  preserve                    = false
 }
