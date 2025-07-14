7b3bcd7a9c1fee73487092a41c4ad9bc74c4f367
Ulises Larraga <uliseslarraga@gmail.com>
2025-07-13 21:53:30 -0600

    feat: Add arn for worker nodes group

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -3,6 +3,11 @@ variable "tags" {
   description = "Common resource tags"
 }
 
+variable "cluster_name" {
+  description = "Eks cluster name"
+  type        = string
+}
+
 variable "environment" {
   type        = string
   description = "Env name"
@@ -16,4 +21,9 @@ variable "oidc_provider_arn" {
 variable "oidc_provider_url" {
   type        = string
   description = "OIDC provider url"
+}
+
+variable "worker_nodes_arn" {
+  type        = string
+  description = "Worker nodes group arn"
 }
